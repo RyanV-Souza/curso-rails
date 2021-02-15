65d7e81016cd471371b32c7079aca3549d7dc1db
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2021-02-15 20:33:42 -0300

    Fix the system

--- modulo_05/time_to_answer/config/routes.rb
+++ modulo_05/time_to_answer/config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+
   namespace :site do
     get 'welcome/index'
   end
-  namespace :profiles_backoffice do
-    get 'welcome/index'
-  end
+
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-  devise_for :profiles
+  
   devise_for :admins
+  devise_for :users
+
 
   get 'inicio', to: 'site/welcome#index'
   root to: 'site/welcome#index'
