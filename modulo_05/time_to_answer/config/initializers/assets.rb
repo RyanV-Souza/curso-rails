ccbedb6c343cc491b47a602fb5f5a539750091c3
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2021-02-16 15:03:55 -0300

    Adjusting logout button in Admin Pages

--- modulo_05/time_to_answer/config/initializers/assets.rb
+++ modulo_05/time_to_answer/config/initializers/assets.rb
@@ -19,7 +19,8 @@ Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_ba
 # /lib/assets
 Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css 
                                                   custom.js custom.css
-                                                  img.jpg)
+                                                  img.jpg
+                                                  undraw_profile.svg)
 
 # /vendor/assets
 Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.js )
