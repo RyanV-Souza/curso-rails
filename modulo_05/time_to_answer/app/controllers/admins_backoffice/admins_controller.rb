ab366cb7bb8a6f4ee4d71ae2534399d70e539893
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2021-02-17 18:02:16 -0300

    Enabling the notice with Bootstrap Growl

--- modulo_05/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
+++ modulo_05/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
@@ -7,6 +7,20 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
     @admins = Admin.all
   end
 
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(params_admin)
+
+    if @admin.save
+      redirect_to admins_backoffice_admins_path, notice: "Administrador cadastrado com sucesso!"
+    else
+      render :new  
+    end
+  end
+
   def edit
   end
 
@@ -19,7 +33,7 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   end
 
   private
-  
+
   def verify_password
     if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
       params[:admin].extract!(:password, :password_confirmation)
