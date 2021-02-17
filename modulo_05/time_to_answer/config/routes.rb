7beed8d3e4d5af84da8707101f39f2f767b26188
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2021-02-17 15:40:01 -0300

    Adjusting the route: edit

--- modulo_05/time_to_answer/config/routes.rb
+++ modulo_05/time_to_answer/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :admins_backoffice do
-    resources :admins, only: [:index]
-  end
+
   namespace :users_backoffice do
     get 'welcome/index'
   end
@@ -11,7 +9,8 @@ Rails.application.routes.draw do
   end
 
   namespace :admins_backoffice do
-    get 'welcome/index'
+    get 'welcome/index' #Dashboard
+    resources :admins, only: [:index, :edit] #Administradores
   end
   
   devise_for :admins
